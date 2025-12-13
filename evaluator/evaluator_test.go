2428f5ecf4e31441d60ad0cc267e7b49a29e1558
ChinmayNakwa <chinmaynakwa29012005@gmail.com>
2025-12-13 19:06:38 +0530

    feat: Environment for tracking values

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -11,8 +11,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment();
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 //Integer
@@ -235,6 +236,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+        },
 	}
 
 	for _, tt := range tests {
@@ -249,5 +254,21 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+        {"let a = 5 * 5; a;", 25},
+        {"let a = 5; let b = a; b;", 5},
+        {"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests { 
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
