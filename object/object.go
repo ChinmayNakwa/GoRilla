302b7dfae1a5d52b15f895706fa1b31a68127072
ChinmayNakwa <chinmaynakwa29012005@gmail.com>
2025-12-15 19:25:25 +0530

    feat: Built-in function len

--- object/object.go
+++ object/object.go
@@ -21,6 +21,7 @@ const (
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "FUNCTION"
 	STRING_OBJ = "STRING"
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 //Integer
@@ -94,3 +95,13 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string { return s.Value}
+
+//Bulit-in Functions
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string { return "builtin function" }
