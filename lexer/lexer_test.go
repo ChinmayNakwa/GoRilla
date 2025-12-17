d08fd4dfcd1fad3fa50e089e3fa95070f0d564b9
ChinmayNakwa <chinmaynakwa29012005@gmail.com>
2025-12-18 03:43:27 +0530

    add: array parsing

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,42 +5,42 @@ import (
 	"GoRilla/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `=+-*(){},;`
+// func TestNextToken(t *testing.T) {
+// 	input := `=+-*(){},;`
 
-	tests := []struct {
-		expectedType token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.SUBSTRACT, "-"},
-		{token.MULTIPLY, "*"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
+// 	tests := []struct {
+// 		expectedType token.TokenType
+// 		expectedLiteral string
+// 	}{
+// 		{token.ASSIGN, "="},
+// 		{token.PLUS, "+"},
+// 		{token.SUBSTRACT, "-"},
+// 		{token.MULTIPLY, "*"},
+// 		{token.LPAREN, "("},
+// 		{token.RPAREN, ")"},
+// 		{token.LBRACE, "{"},
+// 		{token.RBRACE, "}"},
+// 		{token.COMMA, ","},
+// 		{token.SEMICOLON, ";"},
+// 		{token.EOF, ""},
+// 	}
 
-	l := New(input)
+// 	l := New(input)
 
-	for i, tt := range tests {
-		tok := l.NextToken()
+// 	for i, tt := range tests {
+// 		tok := l.NextToken()
 
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-			i, tt.expectedType, tok.Type)
-		}
+// 		if tok.Type != tt.expectedType {
+// 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+// 			i, tt.expectedType, tok.Type)
+// 		}
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got%q",
-			i, tt.expectedLiteral, tok.Literal)
-		}
-	}
-}
+// 		if tok.Literal != tt.expectedLiteral {
+// 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got%q",
+// 			i, tt.expectedLiteral, tok.Literal)
+// 		}
+// 	}
+// }
 
 func TestNextToken(t *testing.T) {
         input := `let five = 5;
@@ -63,6 +63,7 @@ func TestNextToken(t *testing.T) {
         10 != 9;
         "foobar"
         "foo bar"
+        [1, 2];
         `      
 	tests := []struct {
 		expectedType token.TokenType
@@ -144,8 +145,14 @@ func TestNextToken(t *testing.T) {
         {token.SEMICOLON, ";"},
         {token.STRING, "foobar"},
         {token.STRING, "foo bar"},
+        {token.LBRACKET, "["},
+        {token.INT, "1"},
+        {token.COMMA, ","},
+        {token.INT, "2"},
+        {token.RBRACKET, "]"},
+        {token.SEMICOLON, ";"},
         {token.EOF, ""},
-	}
+        }
 
 	
 	l := New(input)
@@ -163,6 +170,6 @@ func TestNextToken(t *testing.T) {
 			i, tt.expectedLiteral, tok.Literal)
 		}
 	}
-
 }
+
 
--- parser/parser.go
+++ parser/parser.go
@@ -92,6 +92,9 @@ func New(l *lexer.Lexer) *Parser {
 	// For strings
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
 
+	// For arrays
+	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+
 	return p
 }
 
@@ -433,7 +436,7 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	exp := &ast.CallExpression{Token: p.curToken, Function: function}
-	exp.Arguments = p.parseCallArguments()
+	exp.Arguments = p.parseExpressionList(token.RPAREN)
 	return exp
 }
 
@@ -465,3 +468,35 @@ func (p *Parser) parseCallArguments() []ast.Expression {
 func (p *Parser) parseStringLiteral() ast.Expression {
 	return &ast.StringLiteral{Token: p.curToken, Value: p.curToken.Literal}
 }
+
+// Arrays
+func (p *Parser) parseArrayLiteral() ast.Expression {
+	array := &ast.ArrayLiteral{Token: p.curToken}
+	array.Elements = p.parseExpressionList(token.RBRACKET)
+	return array
+}
+
+func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
+	list := []ast.Expression{}
+
+	if p.peekTokenIs(end) {
+		p.nextToken()
+		return list
+	}
+
+	p.nextToken()
+	list = append(list, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		list = append(list, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(end) {
+		return nil
+	}
+
+	return list
+}
+
